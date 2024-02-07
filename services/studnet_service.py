d6fd421ab878c67c7d8694f7cb484606c216e4de
andrey <andreydashch@gmail.com>
2024-02-08 00:11:43 +0200

    Parse student attributes

--- services/studnet_service.py
+++ services/studnet_service.py
@@ -6,8 +6,32 @@ def create_student(form) -> Student:
     name = form.get("name")
     email = form.get("email")
     group = form.get("group")
+    student = Student(name, email, True, group)
 
-    return Student(name, email, True, group)
+    student.birth_date = form.get("birth_date")
+    student.birth_place = form.get("birth_place")
+    student.citizenship = form.get("citizenship")
+    student.family_state = form.get("family_state")
+    student.idcard = form.get("idcard")
+
+    student.phone_number = form.get("phone_number")
+    student.home_address = form.get("home_address")
+    student.registration_address = form.get("registration_address")
+
+    student.faculty = form.get("faculty")
+    student.education_level = form.get("education_level")
+    student.education_form = form.get("education_form")
+    student.speciality = form.get("speciality")
+    student.education_program = form.get("education_program")
+
+    student.admission_benefits = form.get("admission_benefits")
+    student.enrolment_order_number = form.get("enrolment_order_number")
+    student.enrolment_order_date = form.get("enrolment_order_date")
+
+    student.school_name = form.get("school_name")
+    student.school_graduation_date = form.get("school_graduation_date")
+
+    return student
 
 
 def save_student(student: Student) -> bool:
