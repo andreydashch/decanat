fdf9ead191c0826112ad2e077fababec17514a51
andrey <andreydashch@gmail.com>
2024-02-08 02:35:30 +0200

    Add encryption for attributes to DB

--- repository/student_repo.py
+++ repository/student_repo.py
@@ -1,11 +1,11 @@
 from entities.student import Student
 from repository.db_conection import Conn
+from repository.encripter.encripter import encrypt, decrypt
 
 conn = Conn()
 
 
 def save_student(student: Student) -> bool:
-
     sql = ("INSERT INTO student(name, email, enable_status, std_group,"
            "birth_date, birth_place, citizenship, family_state, idcard,"
            "phone_number, home_address, registration_address,"
@@ -23,28 +23,28 @@ def save_student(student: Student) -> bool:
         enable_status=student.enable_status,
         group=student.group,
 
-        birth_date=student.birth_date,
-        birth_place=student.birth_place,
-        citizenship=student.citizenship,
-        family_state=student.family_state,
-        idcard=student.idcard,
+        birth_date=encrypt(student.birth_date),
+        birth_place=encrypt(student.birth_place),
+        citizenship=encrypt(student.citizenship),
+        family_state=encrypt(student.family_state),
+        idcard=encrypt(student.idcard),
 
-        phone_number=student.phone_number,
-        home_address=student.home_address,
-        registration_address=student.registration_address,
+        phone_number=encrypt(student.phone_number),
+        home_address=encrypt(student.home_address),
+        registration_address=encrypt(student.registration_address),
 
-        faculty=student.faculty,
-        education_level=student.education_level,
-        education_form=student.education_form,
-        speciality=student.speciality,
-        education_program=student.education_program,
+        faculty=encrypt(student.faculty),
+        education_level=encrypt(student.education_level),
+        education_form=encrypt(student.education_form),
+        speciality=encrypt(student.speciality),
+        education_program=encrypt(student.education_program),
 
-        admission_benefits=student.admission_benefits,
-        enrolment_order_number=student.enrolment_order_number,
-        enrolment_order_date=student.enrolment_order_date,
+        admission_benefits=encrypt(student.admission_benefits),
+        enrolment_order_number=encrypt(student.enrolment_order_number),
+        enrolment_order_date=encrypt(student.enrolment_order_date),
 
-        school_name=student.school_name,
-        school_graduation_date=student.school_graduation_date
+        school_name=encrypt(student.school_name),
+        school_graduation_date=encrypt(student.school_graduation_date)
     )
 
     return conn.exec_sql(sql)
@@ -55,8 +55,17 @@ def get_students_by_group(group):
            "WHERE std_group = '{group}';").format(
         group=group
     )
+    print(encrypt(group))
+    res = conn.exec_select_sql(sql)
 
-    return conn.exec_select_sql(sql)
+    decrypt_res_list = []
+    for student_index in range(len(res)):
+        decrypt_res = list(res[student_index][0:5])
+        for el_index in range(len(res[student_index]) - 5):
+            decrypt_res.append(decrypt(res[student_index][el_index + 5]))
+        decrypt_res_list.append(decrypt_res)
+    print(decrypt_res_list)
+    return decrypt_res_list
 
 
 def get_student_by_id(id):
@@ -65,7 +74,13 @@ def get_student_by_id(id):
         id=id
     )
 
-    return conn.exec_select_sql(sql)[0]
+    res = conn.exec_select_sql(sql)[0]
+
+    decrypt_res = list(res[0:5])
+    for el_index in range(len(res) - 5):
+        decrypt_res.append(decrypt(res[el_index + 5]))
+
+    return decrypt_res
 
 
 def update_student(student):
