8280e9d0480df1308d35da7f9125cd1de8366ea5
andrey <andreydashch@gmail.com>
2024-02-09 03:12:35 +0200

    Add attributes to User

--- entities/user.py
+++ entities/user.py
@@ -1,3 +1,8 @@
 class User:
     def __init__(self, name):
         self.name = name
+        self.role = []
+
+        self.email = None
+        self.google_name = None
+        self.google_profile_pic = None
