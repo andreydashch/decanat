410f06d38cc35ef9e22226ea3e9ae0aa86e75b8d
andrey <andreydashch@gmail.com>
2024-02-14 15:25:59 +0200

    Add update user

--- repository/user_repo.py
+++ repository/user_repo.py
@@ -19,10 +19,24 @@ def save_user(user: User) -> bool:
     return conn.exec_sql(sql)
 
 
-# TODO update_user
+def update_user(user: User) -> bool:
+    sql = ("WITH auth_user as ("
+           "UPDATE  auth_user SET name = '{name}', email = '{email}' WHERE id = {id}"
+           "), del as ("
+           "DELETE FROM auth_user_roles WHERE user_id = {id}"
+           ") "
+           "INSERT INTO auth_user_roles(user_id, role) "
+           "VALUES ({id}, unnest(array[{roles}]));").format(
+        name=user.name,
+        id=user.db_id,
+        email=user.email,
+        roles=user.get_roles_strings()
+    )
+
+    return conn.exec_sql(sql)
 
 
-def get_user_by_email(email):
+def get_user_by_email(email) -> User:
     sql = ("SELECT auth_user.*, array_agg(DISTINCT auth_user_roles.role) "
            "FROM auth_user "
            "LEFT JOIN  auth_user_roles "
@@ -38,3 +52,4 @@ def get_user_by_email(email):
         return None
 
     return res[0]
+
